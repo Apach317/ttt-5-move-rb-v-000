e55d129e335403857302f1067f0d824bed2a1c9a
Ron Lynch <lynch.ronald1@gmail.com>
2018-08-15 20:38:28 +0000

    automatically backed up by learn

--- lib/move.rb
+++ lib/move.rb
@@ -9,13 +9,9 @@ end
 # code your input_to_index and move method here!
 
 def input_to_index(user_input)
-  index = user_input.to_i - 1
+  index = user_input.to_i
 end
 
 def move(board, index, value = "X")
   board[index] = value
-end
-
-def user_info
-  puts "Where would you like to go?"
 end
